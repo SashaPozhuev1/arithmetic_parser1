9abee3767987ac690b05c57dffbeebfeac255bdc
Alexandr Pozhuev <32191131+SashaPozhuev1@users.noreply.github.com>
2017-09-22 22:00:03 +0300

    Parser 1 VCPP
    parcer 1 in Visual Cpp

---
+++ arithmeticparser1.cpp
@@ -0,0 +1,63 @@
+// arithmeticparser1.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+
+
+
+using namespace std;
+
+int main()
+{
+	int a, b, r;
+	char op1, op2;
+	cin >> a;
+	cin >> op1;
+	if ((op1 == '>') || (op1 == '<')) {
+		cin >> op2;
+		cin >> b;
+	}
+	else {
+		cin >> b;
+	}
+	if (op1 == '+') {
+		r = a + b;
+	}
+	else if (op1 == '-') {
+		r = a - b;
+	}
+	else if (op1 == '*') {
+		r = a*b;
+	}
+	else if (op1 == '/') {
+		if (b == 0) { cout << "wrong operation"; }
+		else r = a / b;
+	}
+	else if ((op1 == '<') && (op2 == '<')) {
+		r = a << b;
+	}
+	else if ((op1 == '>') && (op2 == '>')) {
+		r = a >> b;
+	}
+	else if (op1 == '&') {
+		r = a&b;
+	}
+	else if (op1 == '|') {
+		r = a | b;
+	}
+	else if (op1 == '^') {
+		r = a^b;
+	}
+	else if (op1 == '%') {
+		r = a%b;
+	}
+	else {
+		cout << "wrong operation" << endl;
+		system("pause");
+		return -1;
+	}
+	cout << "result: " << r << endl;
+	//cin.get();
+	system("pause");
+	return 0;
+}
